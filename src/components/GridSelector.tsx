5a6e9b1d2a951f48995769e12a635583d628d72a
kentur <kevinxrturop@gmail.com>
2022-09-14 12:10:02 -0500

    Update GridSelector.tsx

--- src/components/GridSelector.tsx
+++ src/components/GridSelector.tsx
@@ -9,7 +9,7 @@ interface GridSelectorProps {
   setGridView: (...arg: any) => void;
 }
 
-export type GridSize = '4x4' | '4x4' | '4x4' | '6x6';
+export type GridSize = '1x1' | '2x2' | '4x4' | '6x6';
 
 function GridSelector({ gridView, setGridView }: GridSelectorProps) {
   return (
@@ -18,26 +18,26 @@ function GridSelector({ gridView, setGridView }: GridSelectorProps) {
         className={clsx(
           'flex w-10 items-center justify-center border-r-2 border-gray-800 md:hidden',
           {
-            'bg-gray-800': gridView === '4x4',
+            'bg-gray-800': gridView === '1x1',
           }
         )}
-        onClick={() => setGridView('4x4')}
+        onClick={() => setGridView('1x1')}
       >
         <SingleGrid
-          className={gridView !== '4x4' ? 'transition hover:scale-110 ' : ''}
-          color={gridView === '4x4' ? 'white' : '#707070'}
+          className={gridView !== '1x1' ? 'transition hover:scale-110 ' : ''}
+          color={gridView === '1x1' ? 'white' : '#707070'}
         />
       </button>
 
       <button
         className={clsx('flex w-10 items-center justify-center', {
-          'bg-gray-800': gridView === '4x4',
+          'bg-gray-800': gridView === '2x2',
         })}
-        onClick={() => setGridView('4x4')}
+        onClick={() => setGridView('2x2')}
       >
         <DoubleGrid
-          className={gridView !== '4x4' ? 'transition hover:scale-110 ' : ''}
-          color={gridView === '4x4' ? 'white' : '#707070'}
+          className={gridView !== '2x2' ? 'transition hover:scale-110 ' : ''}
+          color={gridView === '2x2' ? 'white' : '#707070'}
         />
       </button>
 
