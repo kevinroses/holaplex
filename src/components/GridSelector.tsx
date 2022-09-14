a9a7dba2d7911a907476c4d626310ab92d25e3de
kentur <kevinxrturop@gmail.com>
2022-09-14 15:10:41 -0500

    Update GridSelector.tsx

--- src/components/GridSelector.tsx
+++ src/components/GridSelector.tsx
@@ -9,37 +9,12 @@ interface GridSelectorProps {
   setGridView: (...arg: any) => void;
 }
 
-export type GridSize = '1x1' | '2x2' | '4x4' | '6x6';
+export type GridSize = '4x4' | '6x6';
 
 function GridSelector({ gridView, setGridView }: GridSelectorProps) {
   return (
     <div className="ml-4  hidden divide-gray-800 rounded-lg border-2 border-solid border-gray-800 sm:flex">
-      <button
-        className={clsx(
-          'flex w-10 items-center justify-center border-r-2 border-gray-800 md:hidden',
-          {
-            'bg-gray-800': gridView === '1x1',
-          }
-        )}
-        onClick={() => setGridView('1x1')}
-      >
-        <SingleGrid
-          className={gridView !== '1x1' ? 'transition hover:scale-110 ' : ''}
-          color={gridView === '1x1' ? 'white' : '#707070'}
-        />
-      </button>
-
-      <button
-        className={clsx('flex w-10 items-center justify-center', {
-          'bg-gray-800': gridView === '2x2',
-        })}
-        onClick={() => setGridView('2x2')}
-      >
-        <DoubleGrid
-          className={gridView !== '2x2' ? 'transition hover:scale-110 ' : ''}
-          color={gridView === '2x2' ? 'white' : '#707070'}
-        />
-      </button>
+    
 
       <button
         className={clsx(
