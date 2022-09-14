7f763ea37087cf2fb898103ce5cc93af4845d84e
kentur <kevinxrturop@gmail.com>
2022-09-14 12:21:19 -0500

    Update carrousell.tsx

--- src/pages/profiles/[publicKey]/carrousell.tsx
+++ src/pages/profiles/[publicKey]/carrousell.tsx
@@ -404,13 +404,13 @@ export const NFTGrid: FC<NFTGridProps> = ({
        <div
         className={clsx(
           'grid grid-cols-1 gap-6',
-          gridView === '4x4'
+          gridView === '6x6'
             ? 'md:grid-cols-2'
             : gridView === '4x4'
             ? 'sm:grid-cols-2 lg:grid-cols-3 '
-            : gridView === '4x4'
+            : gridView === '2x2'
             ? 'sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'
-            : gridView === '6x6'
+            : gridView === '1x1'
             ? 'md:grid-cols-6'
             : ''
         )}
