2c4e854733c8f2824129be9b7bb7a8a069cc44b9
kentur <kevinxrturop@gmail.com>
2022-09-14 20:59:13 -0500

    Update MediaQuery.ts

--- src/assets/styles/MediaQuery.ts
+++ src/assets/styles/MediaQuery.ts
@@ -2,4 +2,4 @@ export const MediaQuerySizes = { sm: 576, md: 768, lg: 992, xl: 1200, xxl: 1400 
 export const mq = (key: keyof typeof MediaQuerySizes) =>
   `@media (min-width: ${MediaQuerySizes[key]}px)`;
 
-.grid-cols-1 : { grid-template-columns: repeat(4,minmax(0,1fr));}
+grid-cols-1 : { grid-template-columns: repeat(4,minmax(0,1fr));}
